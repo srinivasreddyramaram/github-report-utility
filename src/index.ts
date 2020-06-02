753094260a9de79e7f4dce78a15f5db925e9430e
SriRam <sramaram@rythmos.com>
2020-06-02 12:53:19 +0530

    read username from command-line

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,19 @@ import { Repo } from "./Repo";
 import * as _ from 'lodash';
 
 let gitHubService = new GithubApiService();
-gitHubService.getUserInfo('srinivasreddyramaram', (user: User) => {
-    //console.log(user);
-    gitHubService.getRepos('srinivasreddyramaram', (repos: Repo[]) => {
-        //console.log(repos);
-        let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.forkCount * -1]);
-        let filteredRepos = _.take(sortedRepos, 5);
-        user.repos = filteredRepos;
-        console.log(user);
+if (process.argv.length < 3) {
+    console.log('Please pass the username as argument');
+} else {
+    let userName = process.argv[2]; //read from the command-line arguments
+    gitHubService.getUserInfo(userName, (user: User) => {
+        gitHubService.getRepos(userName, (repos: Repo[]) => {
+            let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.forkCount * -1]);
+            let filteredRepos = _.take(sortedRepos, 5);
+            user.repos = filteredRepos;
+            console.log(user);
+        });
     });
-});
+}
+
+
 
