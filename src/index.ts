beded4626a4fa5af29c1d97b9d48ab07df043602
SriRam <sramaram@rythmos.com>
2020-06-02 12:37:36 +0530

    sort and filter repos using lodash

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { GithubApiService } from "./GithubApiService";
 import { User } from "./User";
 import { Repo } from "./Repo";
+import * as _ from 'lodash';
 
 let gitHubService = new GithubApiService();
 gitHubService.getUserInfo('srinivasreddyramaram', (user: User) => {
     //console.log(user);
     gitHubService.getRepos('srinivasreddyramaram', (repos: Repo[]) => {
         //console.log(repos);
-        user.repos = repos;
+        let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.forkCount * -1]);
+        let filteredRepos = _.take(sortedRepos, 5);
+        user.repos = filteredRepos;
         console.log(user);
     });
 });
